50a56fe7bcb9da7aacf575b9e832bc19813714dd
Orton Bright <ortonb110@gmail.com>
2023-04-13 22:12:35 +0100

    Working Post and Delete

--- server/app.js
+++ server/app.js
@@ -3,9 +3,16 @@ const app = express();
 const todo = require('./Routes/Routes')
 require('dotenv').config();
 const connectDB = require('./DB/Connect');
+const cors = require('cors');
 
 //Adding Json 
 app.use(express.json());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.use('/api/v1/todo', todo);
 
